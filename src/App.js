9abedf6fa760b4ad8d9a0f5d0793ef98292ff794
shivu1919 <shivamthakur.ss129@gmail.com>
2024-12-05 18:52:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Mobile from './Mobile'
+
+function App(){
+  return(
+    <>
+      <Mobile 
+      src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70"
+      name="Apple iPhone 15 (Black, 128 GB)"
+      price="58100"
+      />
+
+      <Mobile 
+      src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/x/k/h/-original-imah5ywff9rr76zh.jpeg?q=70"
+      name="SAMSUNG Galaxy S23 FE (Graphite, 128 GB)  (8 GB RAM)"
+      price="34999"
+      />
+
+      <Mobile 
+      src="https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/i/p/l/-original-imah2fjcxjdjykpc.jpeg?q=70"
+      name="Motorola G85 5G (Cobalt Blue, 128 GB)  (8 GB RAM)"
+      price="17999"
+      />
+
+    </>
+  );
+}
+
+export default App;
